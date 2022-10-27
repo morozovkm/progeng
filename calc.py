f78f28eb42949c6d1ec6c24ba90f331dc475a422
Kirill Morozov <kmmorozov@gmail.com>
2022-10-27 20:47:54 +0300

    Убрал лишний код из calc.py

--- calc.py
+++ calc.py
@@ -1,11 +1,7 @@
-import pandas as pd
 try:
     a = int(input('Введите первое число:'))
     b = int(input('Введите второе число:'))
     c = a + b
     print(c)
-
 except:
     print('Вы ввели не число!!!')
-
-
